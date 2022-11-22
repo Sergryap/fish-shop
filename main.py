f76da9fb49a5cd6c695fe4ac55e03e5b5b940e2a
sergryap <rs1180@mail.ru>
2022-11-22 22:46:38 +0500

    Added feature to modify the '.env' file if the token is invalid

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
+
 import requests
 from environs import Env
 from pprint import pprint
+from fileinput import FileInput
 
 
 def get_access_token(client_id):
@@ -14,13 +17,26 @@ def get_access_token(client_id):
     return response.json()['access_token']
 
 
-def get_response_api(token, resource):
+def get_response_api(client_id, token, resource):
     access_token = token
     init_url = 'https://api.moltin.com'
     general_url = init_url + resource
-    headers = {'Authorization': f'Bearer {access_token}'}
-    response = requests.get(general_url, headers=headers)
-    response.raise_for_status()
+    status_code = False
+    while not status_code:
+        headers = {'Authorization': f'Bearer {access_token}'}
+        try:
+            response = requests.get(general_url, headers=headers)
+            response.raise_for_status()
+            status_code = response.ok
+        except requests.exceptions.HTTPError as err:
+            access_token = get_access_token(client_id)
+            os.environ['ACCESS_TOKEN'] = access_token
+            for row in FileInput('.env', inplace=True):
+                if row[:2] == 'ACC':
+                    row = f'ACCESS_TOKEN={access_token}'
+                else:
+                    row = row[:-2]
+                print(row)
 
     return response.json()
 
@@ -30,10 +46,10 @@ def main():
     env.read_env()
     client_id = env('CLIENT_ID')
     access_token = env('ACCESS_TOKEN')
-    # access_token = get_access_token(client_id)
-    products = get_response_api(token=access_token, resource='/catalog/products')
+    products = get_response_api(client_id=client_id, token=access_token, resource='/catalog/products')
     pprint(products)
 
 
 if __name__ == '__main__':
     main()
+
