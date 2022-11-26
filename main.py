d4c3150069d735c41b2e3fe96c3933759902aa67
sergryap <rs1180@mail.ru>
2022-11-27 00:14:42 +0500

    Created general call_api_func func

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_cart(token, reference):
     return response.json()
 
 
-def create_cart(token, customer_token, name, description='fish-order'):
+def create_cart(token, name, description='fish-order'):
     url = 'https://api.moltin.com/v2/carts'
     headers = {
         'Authorization': f'Bearer {token}',
@@ -73,6 +73,7 @@ def add_product_to_cart(token, product_id, quantity, reference):
     }
     response = requests.post(url=url, headers=headers, json=json_data)
     response.raise_for_status()
+
     return response.json()
 
 
@@ -97,32 +98,12 @@ def generate_customer_token(token, email, password):
     return response.json()
 
 
-def main():
+def call_api_func(api_func, client_id, client_secret, *args, **kwargs):
     status_code = False
-    env = Env()
-    env.read_env()
-    client_id = env('CLIENT_ID')
-    client_secret = env('CLIENT_SECRET')
-
     while not status_code:
-        access_token = env('ACCESS_TOKEN')
-        customer_token = env('CUSTOMER_TOKEN')
-        customer_id = env('CUSTOMER_ID')
-        customer_email = env('CUSTOMER_EMAIL')
-        customer_password = env('CUSTOMER_PASSWORD')
         try:
             status_code = True
-            # products = get_products(token=access_token)
-            # customer_token = generate_customer_token(token=access_token, email=customer_email, password=customer_password)
-            # cart = create_cart(token=access_token, customer_token=customer_token, name='Cart', description='test-1')
-            # cart = get_cart(token=access_token, reference='Cart')
-            product_to_cart = add_product_to_cart(
-                token=access_token,
-                product_id='4f405bed-cb79-42cc-aeed-4fd8fe83c81c',
-                quantity=1,
-                reference='Cart'
-            )
-            pprint(product_to_cart)
+            result = api_func(*args, **kwargs)
         except requests.exceptions.HTTPError:
             status_code = False
             access_token = get_access_token(client_id, client_secret)
@@ -133,6 +114,31 @@ def main():
                 else:
                     row = row[:-1]
                 print(row)
+    return result
+
+
+def main():
+    env = Env()
+    env.read_env()
+    client_id = env('CLIENT_ID')
+    client_secret = env('CLIENT_SECRET')
+    access_token = env('ACCESS_TOKEN')
+    customer_token = env('CUSTOMER_TOKEN')
+    customer_id = env('CUSTOMER_ID')
+    customer_email = env('CUSTOMER_EMAIL')
+    customer_password = env('CUSTOMER_PASSWORD')
+    # products = call_api_func(get_products, client_id, client_secret, token=access_token)
+    # customer_token = call_api_func(generate_customer_token, client_id, client_secret, token=access_token, email=customer_email, password=customer_password)
+    # cart = call_api_func(create_cart, client_id, client_secret, token=access_token, name='Cart', description='test-1')
+    # cart = call_api_func(get_cart, client_id, client_secret, token=access_token, reference='Cart')
+    product_to_cart = call_api_func(
+        add_product_to_cart, client_id, client_secret,
+        token=access_token,
+        product_id='4f405bed-cb79-42cc-aeed-4fd8fe83c81c',
+        quantity=1,
+        reference='Cart'
+    )
+    pprint(product_to_cart)
 
 
 if __name__ == '__main__':
